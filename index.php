36e02544677670202c80ce51bba8a402dfc20f93
tetsis <hitoshi.zin.nakao@gmail.com>
2016-02-14 02:00:24 +0900

    [mod] file reading from date.dat at rdtimer.js

--- index.php
+++ index.php
@@ -5,23 +5,37 @@
     <!--<meta http-equiv="refresh" content="43200">-->
     <meta http-equiv="refresh" content="5000;URL=index.php">
     <title>研究締め切りタイマー</title>
-    <link rel="stylesheet" type="text/css" href="main.css">
     <script type="text/javascript" src="research_deadline_timer.js"></script>
+    <link rel="stylesheet" type="text/css" href="main.css">
 </head>
 <body>
 
 <?php
 $fp = fopen("admin/date.dat", "r");
 if ($fp) {
-    $shuAbst = fgetcsv($fp);
-    $shuPaper = fgetcsv($fp);
-    $shuPresen = fgetcsv($fp);
-    $sotsuAbst = fgetcsv($fp);
-    $sotsuPaper = fgetcsv($fp);
-    $sotsuPresen = fgetcsv($fp);
+    $timeShuAbst = fgetcsv($fp);
+    $timeShuPaper = fgetcsv($fp);
+    $timeShuPresen = fgetcsv($fp);
+    $timeSotsuAbst = fgetcsv($fp);
+    $timeSotsuPaper = fgetcsv($fp);
+    $timeSotsuPresen = fgetcsv($fp);
 }
 fclose($fp);
+
+$jsonShuAbst = json_encode($timeShuAbst);
+$jsonShuPaper = json_encode($timeShuPaper);
+$jsonShuPresen = json_encode($timeShuPresen);
+$jsonSotsuAbst = json_encode($timeSotsuAbst);
+$jsonSotsuPaper = json_encode($timeSotsuPaper);
+$jsonSotsuPresen = json_encode($timeSotsuPresen);
 ?>
+
+    <script type=text/javascript>var timeShuAbst = <?php echo $jsonShuAbst; ?>;</script>
+    <script type=text/javascript>var timeShuPaper = <?php echo $jsonShuPaper; ?>;</script>
+    <script type=text/javascript>var timeShuPresen = <?php echo $jsonShuPresen; ?>;</script>
+    <script type=text/javascript>var timeSotsuAbst = <?php echo $jsonSotsuAbst; ?>;</script>
+    <script type=text/javascript>var timeSotsuPaper = <?php echo $jsonSotsuPaper; ?>;</script>
+    <script type=text/javascript>var timeSotsuPresen = <?php echo $jsonSotsuPresen; ?>;</script>
 
 <h1>研究締め切りタイマー</h1>
 <br/>
@@ -46,9 +60,9 @@ fclose($fp);
             </div>
             <h3>
             <?php
-            print "提出期限：{$shuAbst[0]}年{$shuAbst[1]}月{$shuAbst[2]}日 {$shuAbst[3]}時";
-            if ($shuAbst[4]) {
-                print "{$shuAbst[4]}分";
+            print "提出期限：{$timeShuAbst[0]}年{$timeShuAbst[1]}月{$timeShuAbst[2]}日 {$timeShuAbst[3]}時";
+            if ($timeShuAbst[4]) {
+                print "{$timeShuAbst[4]}分";
             }
             ?>
             </h3>
@@ -58,9 +72,9 @@ fclose($fp);
             </div>
             <h3>
             <?php
-            print "提出期限：{$sotsuAbst[0]}年{$sotsuAbst[1]}月{$sotsuAbst[2]}日 {$sotsuAbst[3]}時";
-            if ($sotsuAbst[4]) {
-                print "{$sotsuAbst[4]}分";
+            print "提出期限：{$timeSotsuAbst[0]}年{$timeSotsuAbst[1]}月{$timeSotsuAbst[2]}日 {$timeSotsuAbst[3]}時";
+            if ($timeSotsuAbst[4]) {
+                print "{$timeSotsuAbst[4]}分";
             }
             ?>
             </h3>
@@ -75,9 +89,9 @@ fclose($fp);
             </div>
             <h3>
             <?php
-            print "提出期限：{$shuPaper[0]}年{$shuPaper[1]}月{$shuPaper[2]}日 {$shuPaper[3]}時";
-            if ($shuPaper[4]) {
-                print "{$shuPaper[4]}分";
+            print "提出期限：{$timeShuPaper[0]}年{$timeShuPaper[1]}月{$timeShuPaper[2]}日 {$timeShuPaper[3]}時";
+            if ($timeShuPaper[4]) {
+                print "{$timeShuPaper[4]}分";
             }
             ?>
             </h3>
@@ -87,14 +101,14 @@ fclose($fp);
             </div>
             <h3>
             <?php
-            print "提出期限：{$sotsuPaper[0]}年{$sotsuPaper[1]}月{$sotsuPaper[2]}日 {$sotsuPaper[3]}時";
-            if ($sotsuPaper[4]) {
-                print "{$sotsuPaper[4]}分";
+            print "提出期限：{$timeSotsuPaper[0]}年{$timeSotsuPaper[1]}月{$timeSotsuPaper[2]}日 {$timeSotsuPaper[3]}時";
+            if ($timeSotsuPaper[4]) {
+                print "{$timeSotsuPaper[4]}分";
             }
             ?>
             </h3>
         </td>
-    </td>
+    </tr>
     <tr>
         <td>
             <h2>発表</h2>
@@ -104,13 +118,13 @@ fclose($fp);
             </div>
             <h3>
             <?php
-            print "発表時間：{$shuPresen[0]}年{$shuPresen[1]}月{$shuPresen[2]}日 {$shuPresen[3]}時";
-            if ($shuPresen[4]) {
-                print "{$shuPresen[4]}分";
+            print "発表時間：{$timeShuPresen[0]}年{$timeShuPresen[1]}月{$timeShuPresen[2]}日 {$timeShuPresen[3]}時";
+            if ($timeShuPresen[4]) {
+                print "{$timeShuPresen[4]}分";
             }
-            print "～{$shuPresen[5]}時";
-            if ($shuPresen[6]) {
-                print "{$shuPresen[6]}分";
+            print "～{$timeShuPresen[5]}時";
+            if ($timeShuPresen[6]) {
+                print "{$timeShuPresen[6]}分";
             }
             ?>
             </h3>
@@ -120,13 +134,13 @@ fclose($fp);
             </div>
             <h3>
             <?php
-            print "発表時間：{$sotsuPresen[0]}年{$sotsuPresen[1]}月{$sotsuPresen[2]}日 {$sotsuPresen[3]}時";
-            if ($sotsuPresen[4]) {
-                print "{$sotsuPresen[4]}分";
+            print "発表時間：{$timeSotsuPresen[0]}年{$timeSotsuPresen[1]}月{$timeSotsuPresen[2]}日 {$timeSotsuPresen[3]}時";
+            if ($timeSotsuPresen[4]) {
+                print "{$timeSotsuPresen[4]}分";
             }
-            print "～{$sotsuPresen[5]}時";
-            if ($sotsuPresen[6]) {
-                print "{$sotsuPresen[6]}分";
+            print "～{$timeSotsuPresen[5]}時";
+            if ($timeSotsuPresen[6]) {
+                print "{$timeSotsuPresen[6]}分";
             }
             ?>
             </h3>
--- research_deadline_timer.js
+++ research_deadline_timer.js
@@ -1,11 +1,11 @@
-var timeShuAbst = '2016-02-04T16:00:00.000';
-var timeShuPaper = '2016-02-09T15:00:00.000';
-var timeShuPresenStart = '2016-02-12T13:00:00.000';
-var timeShuPresenEnd = '2016-02-12T13:30:00.000';
-var timeSotsuAbst = '2016-02-15T17:00:00.000';
-var timeSotsuPaper = '2016-02-18T15:00:00.000';
-var timeSotsuPresenStart = '2016-02-23T11:10:00.000';
-var timeSotsuPresenEnd = '2016-02-23T12:00:00.000';
+var timeShuAbstStr;
+var timeShuPaperStr;
+var timeShuPresenStartStr;
+var timeShuPresenEndStr;
+var timeSotsuAbstStr;
+var timeSotsuPaperStr;
+var timeSotsuPresenStartStr;
+var timeSotsuPresenEndStr;
 
 var counterShuAbst = 0;
 var counterShuPaper = 0;
@@ -17,6 +17,14 @@ var counterSotsuPresen = 0;
 //ロード関数
 window.addEventListener('load',
     function (event) {
+        timeShuAbstStr = createTimeString(timeShuAbst[0], timeShuAbst[1], timeShuAbst[2], timeShuAbst[3], timeShuAbst[4]);
+        timeShuPaperStr = createTimeString(timeShuPaper[0], timeShuPaper[1], timeShuPaper[2], timeShuPaper[3], timeShuPaper[4]);
+        timeShuPresenStartStr = createTimeString(timeShuPresen[0], timeShuPresen[1], timeShuPresen[2], timeShuPresen[3], timeShuPresen[4])
+        timeShuPresenStartStr = createTimeString(timeShuPresen[0], timeShuPresen[1], timeShuPresen[2], timeShuPresen[5], timeShuPresen[6])
+        timeSotsuAbstStr = createTimeString(timeSotsuAbst[0], timeSotsuAbst[1], timeSotsuAbst[2], timeSotsuAbst[3], timeSotsuAbst[4]);
+        timeSotsuPaperStr = createTimeString(timeSotsuPaper[0], timeSotsuPaper[1], timeSotsuPaper[2], timeSotsuPaper[3], timeSotsuPaper[4]);
+        timeSotsuPresenStartStr = createTimeString(timeSotsuPresen[0], timeSotsuPresen[1], timeSotsuPresen[2], timeSotsuPresen[3], timeSotsuPresen[4])
+        timeSotsuPresenStartStr = createTimeString(timeSotsuPresen[0], timeSotsuPresen[1], timeSotsuPresen[2], timeSotsuPresen[5], timeSotsuPresen[6])
         setInterval(display, 100);
     }
 , false);
@@ -25,32 +33,32 @@ window.addEventListener('load',
 function display() {
     //タイマー表示
     //shu_abst
-    counterShuAbst = displayTimeOfAbstAndPaper(timeShuAbst, 'shu_abst', counterShuAbst);
+    counterShuAbst = displayTimeOfAbstAndPaper(timeShuAbstStr, 'shu_abst', counterShuAbst);
     //shu_paper
-    counterShuPaper = displayTimeOfAbstAndPaper(timeShuPaper, 'shu_paper', counterShuPaper);
+    counterShuPaper = displayTimeOfAbstAndPaper(timeShuPaperStr, 'shu_paper', counterShuPaper);
     //shu_presen
-    counterShuPresen = displayTimeOfPresen(timeShuPresenStart, timeShuPresenEnd, 'shu_presen', counterShuPresen);
+    counterShuPresen = displayTimeOfPresen(timeShuPresenStartStr, timeShuPresenEndStr, 'shu_presen', counterShuPresen);
     //sotsu_abst
-    counterSotsuAbst = displayTimeOfAbstAndPaper(timeSotsuAbst, 'sotsu_abst', counterSotsuAbst);
+    counterSotsuAbst = displayTimeOfAbstAndPaper(timeSotsuAbstStr, 'sotsu_abst', counterSotsuAbst);
     //sotsu_paper
-    counterSotsuPaper = displayTimeOfAbstAndPaper(timeSotsuPaper, 'sotsu_paper', counterSotsuPaper);
+    counterSotsuPaper = displayTimeOfAbstAndPaper(timeSotsuPaperStr, 'sotsu_paper', counterSotsuPaper);
     //sotsu_presen
-    counterSotsuPresen = displayTimeOfPresen(timeSotsuPresenStart, timeSotsuPresenEnd, 'sotsu_presen', counterSotsuPresen);
+    counterSotsuPresen = displayTimeOfPresen(timeSotsuPresenStartStr, timeSotsuPresenEndStr, 'sotsu_presen', counterSotsuPresen);
 
     //オワタ表示
     var flagTimeOver = 0;
     //shu_abst
-    flagTimeOver += isTimerOver(timeShuAbst);
+    flagTimeOver += isTimerOver(timeShuAbstStr);
     //shu_paper
-    flagTimeOver += isTimerOver(timeShuPaper);
+    flagTimeOver += isTimerOver(timeShuPaperStr);
     //shu_presen
-    flagTimeOver += isTimerOver(timeShuPresenEnd);
+    flagTimeOver += isTimerOver(timeShuPresenEndStr);
     //sotsu_abst
-    flagTimeOver += isTimerOver(timeSotsuAbst);
+    flagTimeOver += isTimerOver(timeSotsuAbstStr);
     //sotsu_paper
-    flagTimeOver += isTimerOver(timeSotsuPaper);
+    flagTimeOver += isTimerOver(timeSotsuPaperStr);
     //sotsu_presen
-    flagTimeOver += isTimerOver(timeSotsuPresenEnd);
+    flagTimeOver += isTimerOver(timeSotsuPresenEndStr);
 
     if (flagTimeOver == 6) {
         document.getElementById('table').style.display = 'none';
@@ -110,9 +118,9 @@ function displayTime(lap, element, counter) {
     var HOUR_MILLISECOND = 60 * MINUTE_MILLISECOND;
     var DAY_MILLISECOND = 24 * HOUR_MILLISECOND;
 
-    var date = Math.floor(lap/DAY_MILLISECOND);
-    var strDate = ('0' + date).slice(-2);
-    lap -= date * DAY_MILLISECOND;
+    var day = Math.floor(lap/DAY_MILLISECOND);
+    var strDay = ('0' + day).slice(-2);
+    lap -= day * DAY_MILLISECOND;
     var hour = Math.floor(lap/HOUR_MILLISECOND);
     var strHour = ('0' + hour).slice(-2);
     lap -= hour * HOUR_MILLISECOND;
@@ -125,7 +133,7 @@ function displayTime(lap, element, counter) {
     var text = document.getElementById(element);
 
     //文字色の変更
-    if (date < 3) {
+    if (day < 3) {
         text.style.color = 'red';
     }
     else {
@@ -135,7 +143,7 @@ function displayTime(lap, element, counter) {
     //点滅
     //counterが0になった時のみ文字色を黒にする
     //ことで点滅に見せる
-    if (date <= 0) {
+    if (day <= 0) {
         if (counter <= 0) {
             text.style.color = 'black';
             if (hour <= 11) {
@@ -153,7 +161,7 @@ function displayTime(lap, element, counter) {
         counter--;
     }
 
-    text.innerHTML = 'あと ' + strDate + ' 日 ' + strHour + ' 時間 ' + strMinute + ' 分 ' + strSecond + ' 秒';
+    text.innerHTML = 'あと ' + strDay + ' 日 ' + strHour + ' 時間 ' + strMinute + ' 分 ' + strSecond + ' 秒';
 
     return counter;
 }
@@ -178,14 +186,14 @@ function displayCurrentTime() {
     var year = ('0' + now.getFullYear()).slice(-4);
     var month = now.getMonth()+1;
     month = ('0' + month).slice(-2);
-    var date = ('0' + now.getDate()).slice(-2);
+    var day = ('0' + now.getDate()).slice(-2);
     var hour = ('0' + now.getHours()).slice(-2);
     var minute = ('0' + now.getMinutes()).slice(-2);
     var second = ('0' + now.getSeconds()).slice(-2);
 
     var text = document.getElementById('now');
 
-    text.innerHTML ='現在時刻：' + year + ' 年 ' + month + ' 月 ' + date + ' 日 ' + hour + ' 時 ' + minute + ' 分 ' + second + ' 秒';
+    text.innerHTML ='現在時刻：' + year + ' 年 ' + month + ' 月 ' + day + ' 日 ' + hour + ' 時 ' + minute + ' 分 ' + second + ' 秒';
 }
 
 //タイマーの終了を判断する関数（返り値 0：終了していない、1：終了）
@@ -199,4 +207,16 @@ function isTimerOver(timeStr) {
     }
 
     return 0;
+}
+
+//PHPから送られてきた時間変数からJavaScript用時間文字列を生成する関数（返り値 文字列）
+function createTimeString(year, month, day, hour, minute) {
+    year = ('0' + year).slice(-4);
+    month = ('0' + month).slice(-2);
+    day = ('0' + day).slice(-2);
+    hour = ('0' + hour).slice(-2);
+    minute = ('0' + minute).slice(-2);
+    timeStr = year + '-' + month + '-' + day + 'T' + hour + ':' + minute + ':00.000';
+
+    return timeStr;
 }
